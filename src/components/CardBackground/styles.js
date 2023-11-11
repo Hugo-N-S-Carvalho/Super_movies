850ea1fe111e975b040362df1469a1cebfdb4bf4
Hugo-N-S-Carvalho <huggones@hotmail.com>
2023-11-11 08:12:13 -0300

    responsive

--- src/components/CardBackground/styles.js
+++ src/components/CardBackground/styles.js
@@ -166,12 +166,14 @@ p{
 
 export const Poster = styled.div`
 z-index: 2;
-max-width: 13em;
-max-height: 20em;
+max-width: 20vw;
+min-width: 15vw;
+max-height: 30em;
 border-radius: 40px;
 overflow: hidden; /* Adicionamos um overflow para esconder qualquer conteúdo que ultrapasse o limite do Poster */
 box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); /* Adicionamos uma sombra para dar destaque ao poster */
 margin-top: 20px;
+
 img {
 
     width: 15em;
@@ -186,7 +188,7 @@ img {
 
 
 @media (max-width: 768px) {
-    max-width: 50vh;
+    max-width: 50vw;
 max-height: 30vh;
 margin-top: 100px;
 margin-right: 50px;
